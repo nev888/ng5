b8ed6cbe8e1e14a181202d3f9ce42735c146825e
farhad <eng.se.faaaziz@gmail.com>
2018-07-05 14:04:40 +0200

    Fixing lenght

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -45,12 +45,13 @@ export class HomeComponent implements OnInit {
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
-    this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
   }
 
   removeItem(i) {
     this.goals.splice(i, 1);
     this._data.changeGoal(this.goals);
+    this.itemCount = this.goals.length;
     }
 }
